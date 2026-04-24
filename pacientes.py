8fd66c8cc94e8fa19a6522c90d8a958c12c8a7b4
Carlos Benigin <124741524+Cemmb@users.noreply.github.com>
2026-04-24 17:51:24 -0300

    Update pacientes.py

--- pacientes.py
+++ pacientes.py
@@ -4,6 +4,14 @@ paciente_exemplo = {
     "gestante": False,
     "deficiencia": False,
     "leituras":[
+         {
+            "hora": "13:30",
+            "spo2": 90,
+            "temperatura": 38,
+            "escala_dor": 2,
+            "pulso_presente": True,
+            "respirando": True
+        },
         {
             "hora": "14:00",
             "spo2": 95,
